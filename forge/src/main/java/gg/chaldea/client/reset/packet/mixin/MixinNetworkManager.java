df61387850d5104997e610edb3d38db39a7818fe
JustRed23 <manuel.bormans@gmail.com>
2023-05-15 20:11:52 +0200

    Fixed some stuff

--- forge/src/main/java/gg/chaldea/client/reset/packet/mixin/MixinNetworkManager.java
+++ forge/src/main/java/gg/chaldea/client/reset/packet/mixin/MixinNetworkManager.java
@@ -4,18 +4,13 @@ import io.netty.channel.Channel;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.DisconnectedScreen;
 import net.minecraft.client.gui.screens.GenericDirtMessageScreen;
-import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
-import net.minecraft.client.gui.screens.multiplayer.WarningScreen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.internal.TextComponentMessageFormatHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -38,12 +33,14 @@ public class MixinNetworkManager {
             this.channel.close().awaitUninterruptibly();
             this.disconnectedReason = p_150718_1_;
 
-            if (Minecraft.getInstance().screen instanceof GenericDirtMessageScreen ) {
-                Component title = Minecraft.getInstance().screen.getTitle();
+            if (Minecraft.getInstance().screen instanceof GenericDirtMessageScreen dirtMessageScreen) {
+                Component title = dirtMessageScreen.getTitle();
 
-                if (title instanceof TranslatableComponent &&
-                        ((TranslationTextComponent) title).getKey().equals("connect.negotiating")) {
-                    Minecraft.getInstance().setScreen(new DisconnectedScreen(new ServerSelectionList(new MainMenuScreen()), new StringTextComponent(""), this.disconnectedReason));
+                if (title.getContents() instanceof TranslatableContents translatable) {
+                    if (translatable.getKey().equals("connect.negotiating"))
+                        Minecraft.getInstance().setScreen(
+                                new DisconnectedScreen(
+                                        new JoinMultiplayerScreen(new TitleScreen()), Component.literal(""), disconnectedReason));
                 }
             }
         }
